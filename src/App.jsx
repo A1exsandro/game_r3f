7461e716858c776be7c40a67aef7e8874ec3ff46
A1exsandro <alexsandro.acp@gmail.com>
2023-05-21 15:26:33 -0400

    feat: use torii 3d in the app

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ import { Physics } from "@react-three/rapier";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
+    <Canvas shadows camera={{ position: [0, 6, 14], fov: 42 }}>
+      <color attach="background" args={["#dbecfb"]} />
+      <fog attach="fog" args={["#dbecfb", 30, 40]} />
       <Suspense>
         <Physics debug>
           <Experience />
