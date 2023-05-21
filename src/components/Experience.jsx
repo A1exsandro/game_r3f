d9ce38f27cf3708439bbb3fe695c50f161c330ab
A1exsandro <alexsandro.acp@gmail.com>
2023-05-21 15:02:07 -0400

    feat: platform 3d

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,26 @@
-import { OrbitControls } from "@react-three/drei";
+import { Cylinder, OrbitControls } from "@react-three/drei";
+import { CylinderCollider, RigidBody } from "@react-three/rapier";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <meshNormalMaterial />
-        <boxBufferGeometry />
-      </mesh>
+      {/* LIGHTS */}
+      <ambientLight intensity={1} />
+      <directionalLight 
+        position={[5, 5, 5]}
+        intensity={0.8}
+        castShadow
+        color={"#9e69da"}
+      />
+
+      {/* STAGE */}
+      <RigidBody colliders={false} type="fixed" position={-0.5}>
+        <CylinderCollider args={[1/2, 5]} />
+        <Cylinder scale={[5, 1, 5]} receiveShadow>
+          <meshStandardMaterial color="white" />
+        </Cylinder>
+      </RigidBody>
     </>
   );
 };
